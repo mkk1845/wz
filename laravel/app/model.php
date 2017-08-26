84617f351df400d689d30e1d7162c703c8b9315d
mxk <877092426@qq.com>
2017-08-26 14:48:52 +0800

    Initial commit

---
+++ laravel/app/model.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model as BoseModel;
+
+class Model extends BoseModel
+{
+    protected $guarded=[''];
+    // protected $fillable=['title','content'];
+}
---
+++ laravel/routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//用户模块
+//登陆页面
+Route::get('/login', '\App\Http\Controllers\LoginController@index');
+//登陆行为
+Route::post('/login','\App\Http\Controllers\LoginController@login');
+//登出行为
+Route::get('/logout','\App\Http\Controllers\LoginController@logout');
+
+//注册页面
+Route::get('/register','\App\Http\Controllers\RegisterController@index');
+
+//注册行为
+Route::post('/register','\App\Http\Controllers\RegisterController@register');
+
+//个人设置页面
+Route::get('/user/me/setting','\App\Http\Controllers\UserController@setting');
+
+//个人设置操作
+Route::post('/user/me/setting','\App\Http\Controllers\UserController@settingStore');
+
+//文章列表
+Route::get('/posts','\App\Http\Controllers\PostController@index');
+
+//创建文章
+Route::get('posts/create','\App\Http\Controllers\PostController@create');
+Route::post('/posts','\App\Http\Controllers\PostController@store');
+
+//文章详情
+Route::get('/posts/{post}','\App\Http\Controllers\PostController@show');
+
+
+//编辑文章
+Route::get('/posts/{post}/edit','\App\Http\Controllers\PostController@edit');
+Route::put('/posts/{post}','\App\Http\Controllers\Pos   tController@update');
+//删除文章
+Route::get('/posts/{post}/delete','\App\Http\Controllers\PostController@delete');
+
+
+//图片上传
+Route::post('/posts/image/upload','\App\Http\Controllers\PostController@imageUpload');
+
+
+// 提交评论
+Route::post('/posts/{post}/comment','\App\Http\Controllers\PostController@comment');
+
+Route::get('/posts/{post}/zan', '\App\Http\Controllers\PostController@zan');
+Route::get('/posts/{post}/unzan', '\App\Http\Controllers\PostController@unzan');
+
+
